a91f5820d27b1be6f919d4bccba8485196649c92
camilo <cavb.1205@hotmail.com>
2020-06-22 20:18:17 -0400

    se creo opciones del home-contenido

--- adoption/admin.py
+++ adoption/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-@admin.register(Pet,Sexo,Ubicacion,Estado_Adopcion,Tamano,Esterilizado,Color,Raza,Tipo_Pet)
+@admin.register(Pet,Sexo,Ubicacion,Estado_Adopcion,Tamano,Esterilizado,Color,Raza,Tipo_Pet,Desparasitado, Cuidados, Salud, Vacuna )
 class PetAdmin(admin.ModelAdmin):
     pass
--- adoption/models.py
+++ adoption/models.py
@@ -66,6 +66,38 @@ class Ubicacion(models.Model):
     def __str__(self):
         return self.ubicacion
 
+class Vacuna(models.Model):
+    '''vacuna pet'''
+    vacuna = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return self.vacuna
+
+class Desparasitado(models.Model):
+    
+    desparasitado = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return self.desparasitado
+
+
+class Salud(models.Model):
+
+    estado_salud = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.estado_salud
+
+class Cuidados(models.Model):
+
+    nombre_cuidado = models.CharField(max_length=255)
+    descripcion_cuidado = models.TextField()
+
+    def __str__(self):
+        return self.nombre_cuidado
 
 class Pet(models.Model):
     nombre = models.CharField(max_length=100, verbose_name='Nombre de la Mascota')
@@ -76,13 +108,18 @@ class Pet(models.Model):
     imagen2 = models.ImageField(upload_to='images/adoption/',blank=True,null=True)
     fecha_publicacion = models.DateField(auto_now_add=True)
     tipo_pet = models.ForeignKey(Tipo_Pet, on_delete=models.CASCADE)
+    tamano = models.ForeignKey(Tamano, on_delete=models.CASCADE)
     raza = models.ForeignKey(Raza, on_delete=models.CASCADE)
     sexo = models.ForeignKey(Sexo, on_delete=models.CASCADE, null=True)
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
     esterilizado = models.ForeignKey(Esterilizado, on_delete=models.CASCADE)
-    tamano = models.ForeignKey(Tamano, on_delete=models.CASCADE)
+    vacunado = models.ForeignKey(Vacuna, on_delete=models.CASCADE, null=True)
+    desparasitado = models.ForeignKey(Desparasitado, on_delete=models.CASCADE, null=True)
+    chip = models.CharField(max_length=10, null=True,blank=True)
     ubicacion = models.ForeignKey(Ubicacion, on_delete=models.CASCADE)
-    estado_adopcion = models.ForeignKey(Estado_Adopcion, on_delete=models.CASCADE)
+    estado_adopcion = models.ForeignKey(Estado_Adopcion, on_delete=models.CASCADE, null=True)
+    estado_salud = models.ForeignKey(Salud, on_delete=models.Model, null=True)
+    cuidados_especiales = models.ForeignKey(Cuidados, on_delete=models.Model, null=True)
     #usuario = models.ForeignKey(User,null=True,blank=True)
     usuario = usuarios[0]
 
